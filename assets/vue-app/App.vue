2c18bb2d1559ed466292ab4f5bcbd2b55a28f9f7
Egor Cherepanov <chegor92@gmail.com>
2020-03-04 13:35:54 +0700

    Move Navbar to a separate component

--- assets/vue-app/App.vue
+++ assets/vue-app/App.vue
@@ -1,41 +1,25 @@
 <template>
-  <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-              aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <div id="app">
+    <Navbar/>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/about">About</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <main role="main" class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <div>
-          <router-view/>
-        </div>
-      </div>
-
+    <main role="main" class="container mt-5">
+      <router-view/>
     </main><!-- /.container -->
-  </body>
+  </div>
 </template>
 
-<style lang="scss">
-@import "~bootstrap/scss/bootstrap";
+<script>
+  import Navbar from './components/Navbar';
 
-.router-link-exact-active {
-  color: #fff !important;
-}
+  export default {
+    components: {
+      Navbar
+    }
+  };
+</script>
+
+<style lang="scss">
+  .router-link-exact-active {
+    color: #fff !important;
+  }
 </style>
