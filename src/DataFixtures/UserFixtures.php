42f1c8eb8a131e2f443a801dbe23bdbbee54c9a8
Egor Cherepanov <chegor92@gmail.com>
2020-03-14 17:51:38 +0700

    Add non-admin users to fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,12 +18,33 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user
+        $admin = new User();
+        $admin
             ->setEmail('admin@example.com')
-            ->setPassword($this->passwordEncoder->encodePassword($user, 'admin'))
+            ->setPassword($this->passwordEncoder->encodePassword($admin, 'admin'))
             ->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
-        $manager->persist($user);
+        $manager->persist($admin);
+
+        $user1 = new User();
+        $user1
+            ->setEmail('user1@example.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user1, 'user1'))
+            ->setRoles(['ROLE_USER']);
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2
+            ->setEmail('user2@example.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user2, 'user2'))
+            ->setRoles(['ROLE_USER']);
+        $manager->persist($user2);
+
+        $user3 = new User();
+        $user3
+            ->setEmail('user3@example.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user3, 'user3'))
+            ->setRoles(['ROLE_USER']);
+        $manager->persist($user3);
 
         $manager->flush();
     }
